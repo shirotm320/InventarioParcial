1e99da06a1b43d5adbc0b012b128cec0d89521a1
FelixM320 <eliamartines320@gmail.com>
2025-11-26 18:54:36 -0600

    imagen

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO Categories (Name, Description) VALUES 
+('Línea Blanca', 'Refrigeradoras, estufas, lavadoras y secadoras.'),
+('Tecnología', 'Laptops, tablets, impresoras y accesorios de cómputo.'),
+('Audio y Video', 'Televisores Smart TV, barras de sonido, parlantes y home theater.'),
+('Telefonía', 'Smartphones Android, iPhone y accesorios móviles.'),
+('Muebles', 'Juegos de sala, comedores, reclinables y centros de entretenimiento.'),
+('Camas y Colchones', 'Camas matrimoniales, king size, imperiales y colchones ortopédicos.'),
+('Motos', 'Motocicletas de trabajo, deportivas, scooters y cuatrimotos.'),
+('Pequeños Electrodomésticos', 'Licuadoras, cafeteras, microondas, planchas y ollas arroceras.'),
+('Climatización', 'Aires acondicionados split, inverter y ventiladores.'),
+('Óptica', 'Lentes de sol, aros oftálmicos y accesorios visuales.');
